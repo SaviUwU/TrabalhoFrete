d195e39fe948ed0a5c901f936bcb1734349177cb
savio <savio@DESKTOP-OBBEC3J>
2024-08-14 17:11:11 -0300

    Adicionar arquivos de projeto.

---
+++ TrabalhoFrete/ClasseFrete/Classdadosfrete.cs
@@ -0,0 +1,15 @@
+﻿namespace TrabalhoFrete.ClasseFrete
+{
+    public class Classdadosfrete
+    {
+
+
+        public string Nome { get; set; }
+        public float Peso { get; set; }
+        public float Altura { get; set; }
+        public float Largura { get; set; }
+        public float Comprimento { get; set; }
+        public string UF { get; set; }
+
+    }
+}
---
+++ TrabalhoFrete/Controllers/FreteController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrabalhoFrete.ClasseFrete;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FreteController : ControllerBase
+{
+    private const float TaxaPorCm3 = 0.01f;
+
+    private static readonly Dictionary<string, float> TarifaPorEstado = new Dictionary<string, float>
+    {
+        { "SP", 50.0f },
+        { "RJ", 60.0f },
+        { "MG", 55.0f },
+        { "OUTROS", 70.0f }
+    };
+
+    [HttpPost]
+    public IActionResult CalcularFrete([FromBody] Classdadosfrete classdadosfrete)
+    {
+        if (classdadosfrete == null)
+        {
+            return BadRequest("Dados inválidos.");
+        }
+
+        float volume = classdadosfrete.Altura * classdadosfrete.Largura * classdadosfrete.Comprimento;
+        float tarifaEstado = TarifaPorEstado.ContainsKey(classdadosfrete.UF.ToUpper()) ?
+                              TarifaPorEstado[classdadosfrete.UF.ToUpper()] :
+                              TarifaPorEstado["OUTROS"];
+
+        float valorFrete = (volume * TaxaPorCm3) + tarifaEstado;
+
+        return Ok(new
+        {
+            Classdadosfrete = classdadosfrete.Nome,
+            Volume = volume,
+            ValorFrete = valorFrete
+        });
+    }
+}
